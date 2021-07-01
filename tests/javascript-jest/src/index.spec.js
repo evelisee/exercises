e7089d6702c04d25d6a0462e57a55f66a68020c0
evelisee <evelisevazquez@gmail.comm>
2021-07-01 14:43:50 -0300

    feat: adding testes

--- tests/javascript-jest/src/index.spec.js
+++ tests/javascript-jest/src/index.spec.js
@@ -1,9 +1,19 @@
-import { hello } from "./index";
+import { hello, pingPong } from "./index";
 
 // Dica: https://jestjs.io/docs/en/expect
 
 describe("hello", () => {
-  it("should return hello", () => {
+  it("should return 'hello'", () => {
     expect(hello()).toBe("hello");
   });
 });
+
+describe('pingPong',  () => {
+    it("should return 'ping-pong'", () => {
+        expect(pingPong(6)).toBe('ping-pong');
+    });
+
+    it("should return 'ping'", () => {
+        expect(pingPong(3)).toBe('ping');
+    });
+})
