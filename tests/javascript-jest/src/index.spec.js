1e7d13356a73e295457981bd04d6ab07350bd9c1
evelisee <evelisevazquez@gmail.comm>
2021-07-06 15:18:50 -0300

    test: adding tests for pingPong

--- tests/javascript-jest/src/index.spec.js
+++ tests/javascript-jest/src/index.spec.js
@@ -8,12 +8,25 @@ describe("hello", () => {
   });
 });
 
-describe('pingPong',  () => {
-    it("should return 'ping-pong'", () => {
-        expect(pingPong(6)).toBe('ping-pong');
+describe("pingPong", () => {
+  describe("multiple of 2", () => {
+    it("should return ping", () => {
+      expect(pingPong(2)).toBe("ping");
     });
-
-    it("should return 'ping'", () => {
-        expect(pingPong(3)).toBe('ping');
+  });
+  describe("When multiple of 3", () => {
+    it("should return pong", () => {
+      expect(pingPong(3)).toBe("pong");
     });
-})
+  });
+  describe("When multiple of 2 and 3", () => {
+    it("should return ping-pong", () => {
+      expect(pingPong(6)).toBe("ping-pong");
+    });
+  });
+  describe("When is not multiple of 2 and 3", () => {
+    it("should return ping-pong", () => {
+      expect(pingPong(7)).toBe("-");
+    });
+  });
+});
